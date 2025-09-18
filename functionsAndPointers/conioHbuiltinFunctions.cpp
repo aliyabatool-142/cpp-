650266ad5314d77e2dec8fef886226347a0edebc
aliyabatool-142 <aliaraza4591@gmail.com>
2025-09-18 21:19:46 +0500

    Added functionsAndPointers: conioh buuiltin functions and greatest integer function

---
+++ functionsAndPointers/conioHbuiltinFunctions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+    char ch;
+    // cout << "Press any key to continue...";
+    // ch = getch();  // waits for a key press
+    // cout << "\nYou pressed: " << ch;
+
+     cout << "Press a key: ";
+    ch = getche();  // shows the key on screen
+    cout << "\nYou pressed: " << ch;
+    return 0;
+}
+
