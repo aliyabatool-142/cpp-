84964b98c910170fc42d1f49ff2fd66adf0984a2
aliyabatool-142 <aliaraza4591@gmail.com>
2025-09-19 22:23:40 +0500

    Added permutation

---
+++ functionsAndPointers/permutation.cpp
@@ -0,0 +1,24 @@
+/*  nPr = n!/(n-r)!    */
+#include <iostream>
+using namespace std;
+
+int fact(int x){
+    int f =1;
+    for(int i=1; i<=x; i++){
+        f*=i;
+    }
+    return f;
+}
+
+int main(){
+    int n, r;
+    cout<<"Enter n: ";
+    cin>>n ;
+    cout<<"Enter r: ";
+    cin>>r ;
+    int a = fact(n);
+    int b = fact(r);
+    int c = fact(n-r);
+    cout<<a/c;
+    return 0;
+}
