8a619d930fb30454c5e39e3d9a8c1f30faafcca5
aliyabatool-142 <aliaraza4591@gmail.com>
2025-09-20 20:59:32 +0500

    Added reverese string

---
+++ functionsAndPointers/reverseString.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+string reverseString(string str){
+    int n = str.length();
+    for (int i = 0; i<n/2; i++)
+        swap(str[i], str[n-i-1]);
+        return str;
+}
+int main(){
+    string input;
+    cout<<"Enter a string: ";
+    getline(cin,input);
+
+    string reversed = reverseString(input);
+    cout<<"Reversed string = "<<reversed<<endl;
+    return 0;
+}
