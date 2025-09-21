fe9ebba6b874017291ca0dc9ab9b6d79d7e60cac
aliyabatool-142 <aliaraza4591@gmail.com>
2025-09-21 21:10:50 +0500

    Addded sum

---
+++ functionsAndPointers/sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void sum(int a, int b){     //a and b are Formal parameters(box)
+    cout<<a+b;
+}
+
+int main(){
+    sum(23,30);             //actual parameters(values)
+
+}           //this concept is called pass by value
+
+
