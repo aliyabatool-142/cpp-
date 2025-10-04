d7e7814cda37b5e278a3424d982ec7b54e552ff1
aliyabatool-142 <aliaraza4591@gmail.com>
2025-10-04 20:41:12 +0500

    Added basics

---
+++ string/basics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+    string a = "hello ";
+    string b = "guys";
+    string c = a + b;
+    cout<<c;
+
+    // char a[5] = ('h', 'e', 'l', 'l', 'o');
+    // char b [4] = ('g', 'u', 'y', 's');
+    // char c [10]= a + b;
+    // cout<<c;
+
+}
