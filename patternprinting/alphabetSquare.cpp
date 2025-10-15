c153aa6aedca6a1495979b7c7fca1702586690a9
aliyabatool-142 <aliaraza4591@gmail.com>
2025-10-15 22:09:10 +0500

    Added alphabet square

---
+++ patternprinting/alphabetSquare.cpp
@@ -0,0 +1,82 @@
+/*#include <iostream>
+using namespace std;
+int main(){
+
+//rows ->n , columns ->n
+int n;
+cout<<"Enter side of squarer " ;
+cin>>n;
+for(int i = 1 ; i<=n ; i++){
+    for(int j =1; j<=n; j++){
+        cout<<j + 64<<" ";
+    }
+    cout<<endl;
+}
+} */
+
+
+/*#include <iostream>
+using namespace std;
+int main(){
+
+//rows ->n , columns ->n
+int n;
+cout<<"Enter side of squarer " ;
+cin>>n;
+for(int i = 1 ; i<=n ; i++){
+    for(int j =1; j<=n; j++){
+        cout<<(char) (j + 64) <<" ";
+    }
+    cout<<endl;
+}
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main(){
+
+//rows ->n , columns ->n
+int n;
+cout<<"Enter side of squarer " ;
+cin>>n;
+for(int i = 1 ; i<=n ; i++){
+    for(int j =1; j<=n; j++){
+        cout<<(char) (i + 64)<<" ";
+    }
+    cout<<endl;
+}
+}*/
+
+
+/*#include <iostream>
+using namespace std;
+int main(){
+
+//rows ->n , columns ->n
+int n;
+cout<<"Enter side of squarer " ;
+cin>>n;
+for(int i = 1 ; i<=n ; i++){
+    for(int j =1; j<=n; j++){
+        cout<<(char) (j + 96) <<" ";
+    }
+    cout<<endl;
+}
+}*/
+
+
+#include <iostream>
+using namespace std;
+int main(){
+
+//rows ->n , columns ->n
+int n;
+cout<<"Enter side of squarer " ;
+cin>>n;
+for(int i = 1 ; i<=n ; i++){
+    for(int j =1; j<=n; j++){
+        cout<<(char) (i + 96) <<" ";
+    }
+    cout<<endl;
+}
+}
