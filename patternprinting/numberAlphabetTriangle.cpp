6f15b23d6a959c83c8f738195cad662552caba00
aliyabatool-142 <aliaraza4591@gmail.com>
2025-10-18 11:38:21 +0500

    Added number alphabet triangle

---
+++ patternprinting/numberAlphabetTriangle.cpp
@@ -0,0 +1,24 @@
+/*print   1
+          AB
+          123
+          ABCD
+          12345*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 5;
+
+    for(int i =1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            if(i%2 == 0){
+                cout<<(char) (j + 64) ;
+            }else {
+                cout<<j ;
+            }    
+        }
+        cout<<endl;
+    }
+    
+}
+          
