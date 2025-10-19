b1366973b5d12459935ed8fbaa8e91d2c5646854
aliyabatool-142 <aliaraza4591@gmail.com>
2025-10-19 10:53:42 +0500

    Added number square

---
+++ patternprinting/numberSquare.cpp
@@ -0,0 +1,31 @@
+/*#include <iostream>
+using namespace std;
+int main(){
+
+//rows ->n , columns ->n
+int n;
+cout<<"Enter side of squarer " ;
+cin>>n;
+for(int i = 1 ; i<=n ; i++){
+    for(int j =1; j<=n; j++){
+        cout<<j <<" ";
+    }
+    cout<<endl;
+}
+} */
+
+#include <iostream>
+using namespace std;
+int main(){
+
+//rows ->n , columns ->n
+int n;
+cout<<"Enter side of squarer " ;
+cin>>n;
+for(int i = 1 ; i<=n ; i++){
+    for(int j =1; j<=n; j++){
+        cout<<i <<" ";
+    }
+    cout<<endl;
+}
+}
